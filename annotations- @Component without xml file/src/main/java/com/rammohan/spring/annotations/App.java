17b36f22946b817c963bce04efe03edc2960038e
Ram Mohan Reddy Seelam <srammohanreddy23@gmail.com>
2025-03-31 15:32:18 +0530

    @Component annotation without xml file, ComponentScan and Configuration annotations

--- annotations- @Component without xml file/src/main/java/com/rammohan/spring/annotations/App.java
+++ annotations- @Component without xml file/src/main/java/com/rammohan/spring/annotations/App.java
@@ -1,11 +1,11 @@
 package com.rammohan.spring.annotations;
 
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class App {
 	public static void main(String[] args) {
-		ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+		ApplicationContext context = new AnnotationConfigApplicationContext(CollegeConfig.class);
 		College college = context.getBean("collegeBean", College.class);
 		System.out.println(college);
 		college.test();
